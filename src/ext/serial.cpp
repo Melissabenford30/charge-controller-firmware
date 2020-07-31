029007562804aa79e933d124c1efeee1ccbcc9b7
Martin Jäger <martin@libre.solar>
2020-07-31 14:37:50 +0200

    Increase serial watchdog timeout

--- src/ext/serial.cpp
+++ src/ext/serial.cpp
@@ -105,7 +105,7 @@ void process_input(void* user_data)
 void serial_thread()
 {
     uint32_t last_call = 0;
-    int wdt_channel = watchdog_register(50);
+    int wdt_channel = watchdog_register(150);
 
     uart_irq_callback_user_data_set(uart_dev, process_input, NULL);
     uart_irq_rx_enable(uart_dev);
